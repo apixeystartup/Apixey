ff52a4ba09267c4907221e6dfd3c5cc005f48987
apixeystartup <apixeystartup@gmail.com>
2026-08-03 17:39:57 +0530

    feat: add edge middleware to rewrite Wix nav URLs to brainvoice.ai paths

---
+++ middleware.js
@@ -0,0 +1,37 @@
+const WIX_MAP = {
+  '/about': 'https://brainvoiceai.wixstudio.com/home/about',
+  '/blogs': 'https://brainvoiceai.wixstudio.com/home/blogs',
+  '/careers': 'https://brainvoiceai.wixstudio.com/home/careers',
+  '/success-stories': 'https://brainvoiceai.wixstudio.com/home/success-stories',
+  '/get-started': 'https://brainvoiceai.wixstudio.com/home/contact-us',
+};
+
+export const config = {
+  matcher: ['/about', '/blogs', '/careers', '/success-stories', '/get-started'],
+};
+
+export default async function middleware(request) {
+  const { pathname } = request.nextUrl;
+  const wixUrl = WIX_MAP[pathname];
+  if (!wixUrl) return;
+
+  const response = await fetch(wixUrl);
+  let html = await response.text();
+
+  html = html.replace(/https?:\/\/brainvoiceai\.wixstudio\.com\/home\/about/g, '/about');
+  html = html.replace(/https?:\/\/brainvoiceai\.wixstudio\.com\/home\/blogs/g, '/blogs');
+  html = html.replace(/https?:\/\/brainvoiceai\.wixstudio\.com\/home\/careers/g, '/careers');
+  html = html.replace(/https?:\/\/brainvoiceai\.wixstudio\.com\/home\/success-stories/g, '/success-stories');
+  html = html.replace(/https?:\/\/brainvoiceai\.wixstudio\.com\/home\/contact-us/g, '/get-started');
+
+  html = html.replace(/["' ]\/home\/about/g, '"/about');
+  html = html.replace(/["' ]\/home\/blogs/g, '"/blogs');
+  html = html.replace(/["' ]\/home\/careers/g, '"/careers');
+  html = html.replace(/["' ]\/home\/success-stories/g, '"/success-stories');
+  html = html.replace(/["' ]\/home\/contact-us/g, '"/get-started');
+
+  return new Response(html, {
+    status: response.status,
+    headers: { 'content-type': 'text/html; charset=utf-8' },
+  });
+}
