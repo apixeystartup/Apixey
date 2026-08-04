90868dcce77b2e67e1b31ef4cdcc28ad4c7f05dc
apixeystartup <apixeystartup@gmail.com>
2026-08-04 07:27:02 +0530

    fix: Get Started nav link slug changed from get-started to contact-us to match Wix URL

--- api/proxy.js
+++ api/proxy.js
@@ -12,7 +12,7 @@ function buildInjectionScript() {
     { slug: 'blogs', label: 'Blogs' },
     { slug: 'careers', label: 'Careers' },
     { slug: 'success-stories', label: 'Success Stories' },
-    { slug: 'get-started', label: 'Get Started' },
+    { slug: 'contact-us', label: 'Get Started' },
   ];
 
   const navLinksHtml = links.map((l, i) => {
