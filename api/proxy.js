ee6824bd27000ab5009370b30f1dbddf6c13d547
apixeystartup <apixeystartup@gmail.com>
2026-08-07 21:53:20 +0530

    Get started page mobile: decrease wix-wrap height to 1150px (-50px)

--- api/proxy.js
+++ api/proxy.js
@@ -550,7 +550,7 @@ function buildIframePage(slug) {
     #wix-wrap::after{content:"";position:absolute;bottom:0;left:0;right:0;height:50px;background:white;z-index:10;pointer-events:none;}
     ${slug === 'about' ? '#wix-wrap{height:5450px;}@media(max-width:767px){#wix-wrap{height:7940px;}}' : ''}
     ${slug === 'careers' ? '#wix-wrap{height:2450px;margin-top:80px;}@media(max-width:767px){#wix-wrap{height:1350px;margin-top:20px;}#bv-header{padding-bottom:0!important;}}' : ''}
-    ${slug === 'get-started' ? '#wix-wrap{height:3150px;overflow:hidden;}@media(max-width:767px){#wix-wrap{height:1200px!important;}#wix-frame{top:0!important;height:1200px!important;}}' : ''}
+    ${slug === 'get-started' ? '#wix-wrap{height:3150px;overflow:hidden;}@media(max-width:767px){#wix-wrap{height:1150px!important;}#wix-frame{top:0!important;height:1150px!important;}}' : ''}
     #wix-frame{width:100%;height:calc(100% + 50px);border:none;display:block;position:absolute;top:-50px;left:0;}
   </style>
 </head>
