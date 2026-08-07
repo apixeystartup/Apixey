4931a716dffdf90018746e85df225b418fe3796a
apixeystartup <apixeystartup@gmail.com>
2026-08-07 09:07:18 +0530

    About +50 to 5450px

--- api/proxy.js
+++ api/proxy.js
@@ -516,7 +516,7 @@ function buildIframePage(slug) {
 
     /* Iframe container clips Wix banner */
     #wix-wrap{overflow:hidden;position:relative;}
-    ${slug === 'about' ? '#wix-wrap{height:5400px;}' : ''}
+    ${slug === 'about' ? '#wix-wrap{height:5450px;}' : ''}
     ${slug === 'careers' ? '#wix-wrap{height:3150px;}' : ''}
     ${slug === 'get-started' ? '#wix-wrap{height:3150px;}' : ''}
     #wix-frame{width:100%;height:calc(100% + 50px);border:none;display:block;position:absolute;top:-50px;left:0;}
