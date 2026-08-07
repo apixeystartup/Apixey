61d3ceb390763dad7b52e87086cc9110650b5296
apixeystartup <apixeystartup@gmail.com>
2026-08-07 11:11:09 +0530

    Careers page mobile: increase bottom clip to 80px to fully hide Wix Studios banner

--- api/proxy.js
+++ api/proxy.js
@@ -519,7 +519,7 @@ function buildIframePage(slug) {
     /* Iframe container clips Wix banner */
     #wix-wrap{overflow:hidden;position:relative;}
     ${slug === 'about' ? '#wix-wrap{height:5450px;}@media(max-width:767px){#wix-wrap{height:7940px;}}' : ''}
-    ${slug === 'careers' ? 'body{padding-top:0!important;}#wix-wrap{height:2450px;margin-top:100px;overflow:hidden;}#wix-frame{clip-path:inset(0 0 50px 0);}@media(max-width:767px){#wix-wrap{height:1400px;margin-top:80px;overflow:hidden;}#wix-frame{top:0!important;height:1400px!important;clip-path:inset(0 0 40px 0);}}' : ''}
+    ${slug === 'careers' ? 'body{padding-top:0!important;}#wix-wrap{height:2450px;margin-top:100px;overflow:hidden;}#wix-frame{clip-path:inset(0 0 50px 0);}@media(max-width:767px){#wix-wrap{height:1400px;margin-top:80px;overflow:hidden;}#wix-frame{top:0!important;height:1400px!important;clip-path:inset(0 0 80px 0);}}' : ''}
     ${slug === 'get-started' ? '#wix-wrap{height:3150px;}' : ''}
     #wix-frame{width:100%;height:calc(100% + 50px);border:none;display:block;position:absolute;top:-50px;left:0;}
   </style>
