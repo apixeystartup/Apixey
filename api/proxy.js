c60347e434fa804898ceaa4e2ac030c149c1dbc4
apixeystartup <apixeystartup@gmail.com>
2026-08-07 14:45:49 +0530

    Get started page mobile: increase wix-wrap height to 5500px to show FAQ and maps sections

--- api/proxy.js
+++ api/proxy.js
@@ -520,7 +520,7 @@ function buildIframePage(slug) {
     #wix-wrap{overflow:hidden;position:relative;z-index:1;}
     ${slug === 'about' ? '#wix-wrap{height:5450px;}@media(max-width:767px){#wix-wrap{height:7940px;}}' : ''}
     ${slug === 'careers' ? '#wix-wrap{height:2450px;margin-top:80px;}@media(max-width:767px){#wix-wrap{height:1350px;margin-top:20px;}#bv-header{padding-bottom:0!important;}}' : ''}
-    ${slug === 'get-started' ? '#wix-wrap{height:3150px;}' : ''}
+    ${slug === 'get-started' ? '#wix-wrap{height:3150px;}@media(max-width:767px){#wix-wrap{height:5500px;}}' : ''}
     #wix-frame{width:100%;height:calc(100% + 50px);border:none;display:block;position:absolute;top:-50px;left:0;}
   </style>
 </head>
