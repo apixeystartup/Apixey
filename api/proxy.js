c1c8c7b3549ba0b0ec5ca4fc2c77a0320402bd85
apixeystartup <apixeystartup@gmail.com>
2026-08-07 11:27:10 +0530

    Careers page mobile: height 1350px, margin-top 20px, remove top:0 override so iframe uses default top:-50px to hide banner behind header

--- api/proxy.js
+++ api/proxy.js
@@ -519,7 +519,7 @@ function buildIframePage(slug) {
     /* Iframe container clips Wix banner */
     #wix-wrap{overflow:hidden;position:relative;z-index:1;}
     ${slug === 'about' ? '#wix-wrap{height:5450px;}@media(max-width:767px){#wix-wrap{height:7940px;}}' : ''}
-    ${slug === 'careers' ? '#wix-wrap{height:2450px;margin-top:80px;}@media(max-width:767px){#wix-wrap{height:1200px;margin-top:20px;}#wix-frame{top:0!important;height:1200px!important;}}' : ''}
+    ${slug === 'careers' ? '#wix-wrap{height:2450px;margin-top:80px;}@media(max-width:767px){#wix-wrap{height:1350px;margin-top:20px;}}' : ''}
     ${slug === 'get-started' ? '#wix-wrap{height:3150px;}' : ''}
     #wix-frame{width:100%;height:calc(100% + 50px);border:none;display:block;position:absolute;top:-50px;left:0;}
   </style>
