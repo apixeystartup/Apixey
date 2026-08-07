064454c560d7cb7ba38444f7c7b1346b7bda8a4a
apixeystartup <apixeystartup@gmail.com>
2026-08-07 11:23:09 +0530

    Careers page mobile: margin-top 20px, top:0 to prevent content clipping

--- api/proxy.js
+++ api/proxy.js
@@ -519,7 +519,7 @@ function buildIframePage(slug) {
     /* Iframe container clips Wix banner */
     #wix-wrap{overflow:hidden;position:relative;}
     ${slug === 'about' ? '#wix-wrap{height:5450px;}@media(max-width:767px){#wix-wrap{height:7940px;}}' : ''}
-    ${slug === 'careers' ? '#wix-wrap{height:2450px;margin-top:80px;}@media(max-width:767px){#wix-wrap{height:1200px;margin-top:80px;}}' : ''}
+    ${slug === 'careers' ? '#wix-wrap{height:2450px;margin-top:80px;}@media(max-width:767px){#wix-wrap{height:1200px;margin-top:20px;}#wix-frame{top:0!important;height:1200px!important;}}' : ''}
     ${slug === 'get-started' ? '#wix-wrap{height:3150px;}' : ''}
     #wix-frame{width:100%;height:calc(100% + 50px);border:none;display:block;position:absolute;top:-50px;left:0;}
   </style>
